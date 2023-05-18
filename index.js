d36bea246f1aa48f6c1097eda9ce02851d3f9f32
arbayezid <arbayezid0@gmail.com>
2023-05-18 20:24:21 +0600

    post created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 
@@ -27,12 +27,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toyDB').collection('toy')
+
+    app.post('/add-toy', async(req,res) =>{
+        const body = req.body;
+        const result = await toyCollection.insertOne(body)
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
